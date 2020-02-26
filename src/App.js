c020725127cc701b47a530def4a4954fe77db9c7
Benjamin Pernick <bpernick@comcast.net>
2020-02-26 13:04:01 -0600

    sign components into game socket immediately

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import socketIOClient from "socket.io-client";
 const endpoint = '/'
+const socket = socketIOClient(endpoint);
 export default class App extends React.Component {
 
     constructor () {
         super ();
     }
-    onClick(){
-        const socket = socketIOClient(endpoint);
-        socket.emit('getBlackCards', 'oof')
+    componentDidMount (){
         socket.on ('oofRes', ((res) => {console.log(res)}))
         socket.on ('news', ((res) => {console.log(res)}))
+    }
+    onClick(){
+        socket.emit('getBlackCards', 'oof')
     }
     render () {
         return (<h1 onClick = {this.onClick.bind(this)}>Hello World</h1>)
